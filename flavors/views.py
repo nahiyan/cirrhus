02a1d6f0c75bacec8a2324c622f602d7ed5e7842
Nahiyan Alamgir <alnahiyan97@gmail.com>
2018-12-27 08:55:57 +0600

    Now flavors are added by default

--- flavors/views.py
+++ flavors/views.py
@@ -4,8 +4,25 @@ from .models import Flavor
 # Create your views here.
 
 def index (request):
+	flavors = Flavor.objects.all
+
+	if flavors.count() == 0:
+		Flavor.objects.bulk_create(
+			[
+				Flavor(
+					name = "Sine",
+					machine_name = "sine",
+					number_of_input_neurons = 1,
+					number_of_output_neurons = 1),
+				Flavor(
+					name = "Flowers",
+					machine_name = "flowers",
+					number_of_input_neurons = 2,
+					number_of_output_neurons = 1)
+			])
+
 	context = {
-		'flavors': Flavor.objects.all
+		'flavors': flavors
 	}
 
 	return render(request, "flavors/index.html", context)
